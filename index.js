c1e86c5ac8bb15d16743d90ac44ace0e7f811451
kyler <kyler1418@outlook.com>
2022-06-07 14:05:42 -0400

    commit tree 163409389e8070e7c9375918b281418e32ab8532

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(greetingFeet){
+let result
+if (greetingFeet <= 400) {
+  result = 'This one is on me!';}
+  else if(greetingFeet <= 2000) 
+    result = `That will be twenty bucks.`;
+    else if(greetingFeet <= 2500) 
+    result = 'I will gladly take your thirty bucks.';
+    else if(greetingFeet > 2500)
+    result = 'No can do.';
+return result}
+
+
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : 'No go.';
+  
 }
 
 function switchOnCharmFromTip(){
