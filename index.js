f41aa0df5411f7cedd1efdb4eda961ec93a4e6f7
kyler <kyler1418@outlook.com>
2022-06-07 15:24:20 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(greetingFeet){
+let result
+if (greetingFeet <= 400) {
+  result = 'This one is on me!';}
+  else if(greetingFeet <= 2000) 
+    result = `That will be twenty bucks.`;
+    else if(greetingFeet <= 2500) 
+    result = 'I will gladly take your thirty bucks.';
+    else if(greetingFeet > 2500)
+    result = 'No can do.';
+return result}
+
+
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : 'No go.';  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip)
+ const tip = "Generous";
+ switch(tip) {
+   case 'Generous':
+   console.log('Thank you so much.');
+   break;
+   case 'notGenerous':
+   console.log('Thank you.');
+  break;
+  default:
+    console.log('Bye');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
